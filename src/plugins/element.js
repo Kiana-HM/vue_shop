321bb1cb0012691525fe7e39437a3bd65f6b1e2b
Kiana <434473552@qq.com>
2021-07-26 14:58:54 +0800

    完成商品功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -32,7 +32,8 @@ import {
   Tree,
   Alert,
   Tabs,
-  TabPane
+  TabPane, Steps, Step, CheckboxGroup, Checkbox, Upload
+
 } from 'element-ui'
 
 Vue.use(Button)
@@ -65,6 +66,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
